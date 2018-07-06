1db49bbd03fd5564d7025004d1fa93d82c3db76f
polferrando98 <pol.ferrando.98@gmail.com>
2018-07-06 16:21:32 +0200

    Cleaned dist

--- dist/styles.44ef1869.js
+++ dist/styles.44ef1869.js
@@ -169,7 +169,7 @@ module.exports = reloadCSS;
 var reloadCSS = require('_css_loader');
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":1094}],1330:[function(require,module,exports) {
+},{"_css_loader":1094}],1076:[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 
@@ -198,7 +198,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '62769' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '60570' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
@@ -339,4 +339,4 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.parcelRequire, id);
   });
 }
-},{}]},{},[1330], null)
+},{}]},{},[1076], null)
